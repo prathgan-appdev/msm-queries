a33b26cf06ed9fcaf0f8fc0b4e8d734ffa8ce68a
Pratham Gandhi <pratham@uchicago.edu>
2023-02-09 21:26:38 +0000

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/eldest", { :controller => "directors", :action => "wisest" })
+  get("/directors/youngest", { :controller => "directors", :action => "young" })
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
+
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:an_id", { :controller => "movies", :action => "movie_details" })
+
+  get("/actors", { :controller => "actors", :action => "index" })
+  get("/actors/:an_id", { :controller => "actors", :action => "actor_details" })
 end
